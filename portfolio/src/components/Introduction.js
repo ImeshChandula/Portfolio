f1d910ae6193fcf2a4edd438e441a8f31070a0e1
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2023-10-03 08:51:12 +0530

    done

--- portfolio/src/components/Introduction.js
+++ portfolio/src/components/Introduction.js
@@ -1,26 +1,30 @@
-import { FaBlackTie } from "react-icons/fa"
+import { FaHeadset } from "react-icons/fa"
+import { TiTick } from "react-icons/ti"
+import { HiCollection } from "react-icons/hi"
 import "./IntroductionStyle.css"
 
 import React from 'react'
 
 const Introduction = () => {
   return (
-    <div className="intro-container">
+    <div className="introduction">
+      <div className="intro-container">
         <div className="intro">
-            <FaBlackTie size={20} style={{color:"#fff", marginRight:"2rem"}}/>
-            <h3>Experience</h3>
-            <p>2+ Years Experience</p>
+              <HiCollection size={20} style={{color:"#fff", marginRight:"2rem"}}/>
+              <h3>Experience</h3>
+              <p>2+ Years Experience</p>
         </div>
         <div className="intro">
-            <FaBlackTie size={20} style={{color:"#fff", marginRight:"2rem"}}/>
-            <h3>Experience</h3>
-            <p>2+ Years Experience</p>
+              <TiTick size={20} style={{color:"#fff", marginRight:"2rem"}}/>
+              <h3>Completed</h3>
+              <p>10+ Projects</p>
         </div>
         <div className="intro">
-            <FaBlackTie size={20} style={{color:"#fff", marginRight:"2rem"}}/>
-            <h3>Experience</h3>
-            <p>2+ Years Experience</p>
+              <FaHeadset size={20} style={{color:"#fff", marginRight:"2rem"}}/>
+              <h3>Support</h3>
+              <p>Online 7/24</p>
         </div>
+      </div>
     </div>
   )
 }
